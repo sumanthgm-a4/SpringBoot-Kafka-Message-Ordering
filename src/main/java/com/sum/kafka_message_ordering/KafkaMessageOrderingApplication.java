989e9df69bc2e40bb97625b41d13b01d4de8d672
Sumanth GM <214g1a32a4@srit.ac.in>
2026-05-16 10:49:08 +0530

    Initial Commit

---
+++ src/main/java/com/sum/kafka_message_ordering/KafkaMessageOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.sum.kafka_message_ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageOrderingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sum/kafka_message_ordering/KafkaMessageOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.kafka_message_ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessageOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
